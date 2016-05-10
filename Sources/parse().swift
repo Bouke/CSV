0f7105662267dba863fc9bd7cddb0ecb2721c574
Bouke Haarsma <bouke@haarsma.eu>
2016-05-10 22:23:48 +0200

    Ability to skip headers

--- Sources/parse().swift
+++ Sources/parse().swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public func parse(_ data: NSData, delimiter: Character? = nil, quote: Character? = nil) throws -> [[String]] {
+public func parse(_ data: NSData, delimiter: Character? = nil, quote: Character? = nil, skipFirstRow: Bool = false) throws -> [[String]] {
     let parser = Parser()
     if let delimiter = delimiter { parser.delimiter = delimiter }
     if let quote = quote { parser.quote = quote }
@@ -8,7 +8,13 @@ public func parse(_ data: NSData, delimiter: Character? = nil, quote: Character?
     var rows = [[String]]()
     var row = [String]()
     parser.didReadField = { row.append($0) }
-    parser.didReadRow = { _ in rows.append(row); row = [] }
+
+    let appendRow = { (_: Character?) in rows.append(row); row = [] }
+    if skipFirstRow {
+        parser.didReadRow = { _ in row = []; parser.didReadRow = appendRow }
+    } else {
+        parser.didReadRow = appendRow
+    }
 
     try parser.parse(data: data)
 
--- Tests/CSV/ParseTests.swift
+++ Tests/CSV/ParseTests.swift
@@ -8,12 +8,20 @@ class ParseTests: XCTestCase {
         let output = try parse(data)
         XCTAssertEqual(output, [["hello", "world"], ["hello", "gib"]])
     }
+
+    func test_skip_first_row() throws {
+        let csv = "col0,col1\nhello,world\nhello,gib\n"
+        guard let data = csv.data(using: NSUTF8StringEncoding) else { XCTFail(); return }
+        let output = try parse(data, skipFirstRow: true)
+        XCTAssertEqual(output, [["hello", "world"], ["hello", "gib"]])
+    }
 }
 
 extension ParseTests {
     static var allTests : [(String, (ParseTests) -> () throws -> Void)] {
         return [
             ("test_defaults", test_defaults),
+            ("test_skip_first_row", test_skip_first_row),
         ]
     }
 }
