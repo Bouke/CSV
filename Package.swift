3c0b4f71b13a542aaf0d89bee7a91202f5b59747
Bouke Haarsma <bouke@haarsma.eu>
2016-06-28 22:27:13 +0200

    Revert libcsv package overlay rename

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "CSV",
     dependencies: [
-        .Package(url: "https://github.com/Bouke/CLibCSV.git", majorVersion: 1),
+        .Package(url: "https://github.com/Bouke/CCSV.git", majorVersion: 1),
     ]
 )
